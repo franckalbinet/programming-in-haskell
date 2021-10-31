8f844a9b89c8d4acdeb7ad38a592c957c329aa6e
Franck Albinet <franckalbinet@gmail.com>
2021-10-31 11:38:38 +0100

    Last extended example

--- 7-higher-order-functions.hs
+++ 7-higher-order-functions.hs
@@ -127,4 +127,24 @@ winner :: Ord a => [a] -> a
 winner = snd . last . result
 
 -- Alternative vote
+ballots :: [[String]]
+ballots = [["Red","Green"],
+          ["Blue"],
+          ["Green","Blue"],
+          ["Blue","Green"],
+          ["Green"]]
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+               [c] -> c
+               (c:cs) -> winner' (elim c bs)
 
