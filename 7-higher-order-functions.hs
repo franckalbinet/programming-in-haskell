c9948357af8a12d40a3d3531cf1621643666cfa0
Franck Albinet <franckalbinet@gmail.com>
2021-11-01 23:10:04 +0100

    Implement xWhile family

--- 7-higher-order-functions.hs
+++ 7-higher-order-functions.hs
@@ -163,3 +163,20 @@ all' :: (a -> Bool) -> [a] -> Bool
 -- Using composition operator
 all' p  = and . map p
 
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = or . map p
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p [] = []
+takeWhile' p (x:xs) 
+  | p x = x : takeWhile' p xs
+  | otherwise = takeWhile' p []
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p [] = []
+dropWhile' p (x:xs) 
+  | p x = dropWhile' p xs
+  | otherwise = x : xs
+
+
+
