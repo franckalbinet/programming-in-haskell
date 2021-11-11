744307fca321719b101b7e7990bf35b2b9693a2e
Franck Albinet <franckalbinet@gmail.com>
2021-11-11 16:46:38 +0100

    Produces list of logical values

--- 8-declaring-types-and-classes.hs
+++ 8-declaring-types-and-classes.hs
@@ -178,6 +178,27 @@ vars (Not p)     = vars p
 vars (And p q)   = vars p ++ vars q
 vars (Imply p q) = vars p ++ vars q
 
+-- Produces list of logical values
+type Bit = Int
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+bools' :: Int -> [[Bool]]
+bools' n = map (reverse . map conv . make n . int2bin) range 
+    where 
+        range     = [0..(2^n)-1]
+        make n bs = take n (bs ++ repeat 0) 
+        conv 0    = False
+        conv 1    = True
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+    where bss = bools (n-1)
+
+
 
 
 
