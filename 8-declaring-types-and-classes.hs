b305cd3947312a64fbd175267828e84eaa449690
Franck Albinet <franckalbinet@gmail.com>
2021-11-13 19:28:22 +0100

    Finishes the 'Abstract Machine' implementation

--- 8-declaring-types-and-classes.hs
+++ 8-declaring-types-and-classes.hs
@@ -226,12 +226,15 @@ type Cont = [Op]
 
 data Op = EVAL Expr | ADD Int
 
-eval :: Expr -> Cont -> Int
-eval (Val n) c   = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
+eval' :: Expr -> Cont -> Int
+eval' (Val n) c   = exec c n
+eval' (Add x y) c = eval' x (EVAL y : c)
 
+exec :: Cont -> Int -> Int
+exec []           n = n
+exec (EVAL y : c) n = eval' y (ADD n : c)
+exec (ADD n : c)  m = exec c (n+m)
 
-
-
-
+value' :: Expr -> Int
+value' e = eval' e []
 
