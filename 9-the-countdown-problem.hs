06a656d507eeffed8de7fdf698d0306b9dd87df5
Franck Albinet <franckalbinet@gmail.com>
2021-11-30 18:48:07 +0100

    Finishes extented example

--- 9-the-countdown-problem.hs
+++ 9-the-countdown-problem.hs
@@ -1,3 +1,7 @@
+main :: IO()
+--main = print (solutions [1,3,7,10,25,50] 765)
+main = print (solutions' [1,3,7,10,25,50] 765)
+
 -- 9.2 Arithmetic operators
 data Op = Add | Sub | Mul | Div
 
@@ -7,11 +11,11 @@ instance Show Op where
     show Mul = "*"
     show Div = "/"
 
-valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
-valid Sub x y = x > y
-valid Mul _ _ = True
-valid Div x y = x `mod` y == 0
+--valid :: Op -> Int -> Int -> Bool
+--valid Add _ _ = True
+--valid Sub x y = x > y
+--valid Mul _ _ = True
+--valid Div x y = x `mod` y == 0
 
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
@@ -75,5 +79,48 @@ split []     = []
 split [_]    = []
 split (x:xs) = ([x],xs) : [(x:ls,rs) | (ls,rs) <- split xs]
 
+exprs :: [Int] -> [Expr]
+exprs []  = []
+exprs [n] = [Val n]
+exprs ns  = [e | (ls,rs) <- split ns,
+                 l       <- exprs ls,
+                 r       <- exprs rs,
+                 e       <- combine l r]
 
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
 
+ops :: [Op]
+ops = [Add,Sub,Mul,Div]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n =
+    [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+-- solutions [1,3,7,10,25,50] 765
+
+-- 9.8 Combining generation and evaluation
+type Result = (Expr,Int)
+
+results :: [Int] -> [Result]
+results []  = []
+results [n] = [(Val n,n) | n > 0]
+results ns  = [res | (ls,rs) <- split ns,
+                     lx      <- results ls,
+                     ry      <- results rs,
+                     res     <- combine' lx ry]
+
+combine' :: Result -> Result -> [Result]
+combine' (l,x) (r,y) =
+    [(App o l r, apply o x y) | o <- ops, valid o x y]
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n =
+    [e | ns' <- choices ns, (e,m) <- results ns', m == n]
+
+-- 9.9 Exploiting algebraic properties
+valid :: Op -> Int -> Int -> Bool
+valid Add x y = x <= y
+valid Sub x y = x > y
+valid Mul x y = x /= 1 && y /= 1 && x <= y
+valid Div x y = y /= 1 && x `mod` y == 0
