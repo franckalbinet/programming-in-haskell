facfe1423a0b84b269f77b11743fde0e0724576f
Franck Albinet <franckalbinet@gmail.com>
2021-12-10 22:40:31 +0100

    Update

--- 10-interactive-programming.hs
+++ 10-interactive-programming.hs
@@ -13,4 +13,36 @@ getLine' = do x <- getChar
               else 
                   do xs <- getLine'
                      return (x:xs)
-            
+
+putStr' :: String -> IO ()
+putStr' []     = return ()
+putStr' (x:xs) = do putChar x
+                    putStr' xs
+
+putStrLn' :: String -> IO ()
+putStrLn' xs = do putStr' xs
+                  putChar '\n'
+
+strlen :: IO ()
+strlen = do putStr "Enter a string: "
+            xs <- getLine
+            putStr "The string has "
+            putStr (show (length xs))
+            putStrLn " characters"
+
+-- 10.6 Hangman
+hangman :: IO ()
+hangman = do putStrLn "Think of a word:"
+             word <- sgetline
+             putStrLn "Try to guess it:"
+             play word
+
+sgetLine :: IO String
+sgetLine = do x <- getCh
+              if x == '\n` then
+                 do putChar x
+                    return []
+              else
+                 do putChar '-'
+                    xs <-
+               
